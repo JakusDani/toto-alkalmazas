0a8c1871328367e7d1939c1c12eb7dcdf79be8f1
JakusDani <Skelox0@gmail.com>
2019-10-13 14:46:54 +0200

    initial commit.

---
+++ src/toto/tarolo/Talalat.java
@@ -0,0 +1,28 @@
+
+package toto.tarolo;
+
+
+public class Talalat 
+{
+    private int talalatokSzama;
+    private int nyertTalalatokSzama;
+    private int nyeremeny;
+    
+    public void Talalt(int szam, int szam2, int szam3)
+    {
+        
+    }
+
+    public int getTalalatokSzama() {
+        return talalatokSzama;
+    }
+
+    public int getNyertTalalatokSzama() {
+        return nyertTalalatokSzama;
+    }
+
+    public int getNyeremeny() {
+        return nyeremeny;
+    }
+    
+}
